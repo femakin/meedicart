85607de5bef1c3efb4f0abb0f236019e98119a1f
Femi Akinyemi <akinfemi46@gmail.com>
2020-11-25 18:56:00 +0100

    second commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Components/Style/index.css';
+ import './Components/Style/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>,
+,
   document.getElementById('root')
 );
 
